86ba9a6d8cb491cb4055e8e7a5e9f9fdfaa53309
dtsarenko <dimatsarenko89@gmail.com>
2019-02-12 09:44:22 +0200

    Initial application generated by JHipster-5.3.4

---
+++ src/main/java/com/icthh/xm/tmf/ms/document/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package com.icthh.xm.tmf.ms.document.config.audit;
---
+++ src/main/java/com/icthh/xm/tmf/ms/document/web/rest/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring MVC REST controllers.
+ */
+package com.icthh.xm.tmf.ms.document.web.rest;
