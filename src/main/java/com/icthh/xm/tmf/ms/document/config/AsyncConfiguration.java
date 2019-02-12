73ba9c31e376f6d30d330317254612ec67b13bfa
dtsarenko <dimatsarenko89@gmail.com>
2019-02-12 14:07:58 +0200

    deleted unnecessary configs

--- src/main/java/com/icthh/xm/tmf/ms/document/config/AsyncConfiguration.java
+++ src/main/java/com/icthh/xm/tmf/ms/document/config/AsyncConfiguration.java
@@ -15,7 +15,6 @@ import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.SchedulingConfigurer;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.scheduling.config.ScheduledTaskRegistrar;
 
 @Configuration
