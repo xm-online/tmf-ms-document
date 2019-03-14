d70968dabb6a8649b964a8cd56b596ab519c8c83
Dmittry Tsarenko <dimatsarenko89@gmail.com>
2019-03-14 13:22:26 +0200

    add retrieve document lep

---
+++ src/main/java/com/icthh/xm/tmf/ms/document/web/rest/DocumentApiImpl.java
@@ -0,0 +1,28 @@
+package com.icthh.xm.tmf.ms.document.web.rest;
+
+import static java.util.Collections.emptyList;
+
+import com.codahale.metrics.annotation.Timed;
+import com.icthh.xm.commons.lep.LogicExtensionPoint;
+import com.icthh.xm.commons.lep.spring.LepService;
+import com.icthh.xm.tmf.ms.document.web.api.DocumentApiDelegate;
+import com.icthh.xm.tmf.ms.document.web.api.model.Document;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Component;
+
+@Component
+@LepService(group = "service")
+@RequiredArgsConstructor
+public class DocumentApiImpl implements DocumentApiDelegate {
+
+    @PreAuthorize("hasPermission({}, 'DOCUMENT.RETRIEVE')")
+    @Timed
+    @LogicExtensionPoint("Retrieve")
+    @Override
+    public ResponseEntity<List<Document>> retrieveDocument(String id) {
+        return ResponseEntity.ok(emptyList());
+    }
+}
