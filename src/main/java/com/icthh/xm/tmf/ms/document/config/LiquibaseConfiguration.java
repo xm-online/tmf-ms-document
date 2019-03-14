710d605a40875a88f088c435afcbb343255e1d8c
Dmittry Tsarenko <dimatsarenko89@gmail.com>
2019-03-14 13:54:34 +0200

    clean code

--- src/main/java/com/icthh/xm/tmf/ms/document/config/LiquibaseConfiguration.java
+++ src/main/java/com/icthh/xm/tmf/ms/document/config/LiquibaseConfiguration.java
@@ -4,8 +4,7 @@ import io.github.jhipster.config.JHipsterConstants;
 import io.github.jhipster.config.liquibase.AsyncSpringLiquibase;
 import javax.sql.DataSource;
 import liquibase.integration.spring.SpringLiquibase;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.liquibase.LiquibaseProperties;
 import org.springframework.context.annotation.Bean;
@@ -13,13 +12,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.core.task.TaskExecutor;
 
+@Slf4j
 @Configuration
 public class LiquibaseConfiguration {
 
-    private final Logger log = LoggerFactory.getLogger(LiquibaseConfiguration.class);
-
     private final Environment env;
-
 
     public LiquibaseConfiguration(Environment env) {
         this.env = env;
