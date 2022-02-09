6e0a4c668c5fe272461c487f0880d449ef4b30ca
nikita <qazxsw.Uzyn.1590>
2022-02-09 09:46:51 +0200

    feature/MWMVBE-760: add xlsx format for jasper reports

--- src/main/java/com/icthh/xm/tmf/ms/document/service/generation/rendering/jasper/JasperReportsDocumentRenderer.java
+++ src/main/java/com/icthh/xm/tmf/ms/document/service/generation/rendering/jasper/JasperReportsDocumentRenderer.java
@@ -1,6 +1,7 @@
 package com.icthh.xm.tmf.ms.document.service.generation.rendering.jasper;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.springframework.http.MediaType.APPLICATION_OCTET_STREAM;
 import static org.springframework.http.MediaType.APPLICATION_PDF;
 import static org.springframework.http.MediaType.APPLICATION_XML;
 import static org.springframework.http.MediaType.TEXT_XML;
@@ -13,6 +14,8 @@ import com.icthh.xm.tmf.ms.document.service.generation.DocumentRenderer;
 import com.icthh.xm.tmf.ms.document.service.generation.DocumentRendererType;
 import com.icthh.xm.tmf.ms.document.service.generation.rendering.exception.DocumentRenderingException;
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.HashMap;
 import java.util.List;
@@ -23,7 +26,12 @@ import net.sf.jasperreports.engine.JRException;
 import net.sf.jasperreports.engine.JasperExportManager;
 import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.data.JRXlsxDataSource;
 import net.sf.jasperreports.engine.data.JsonDataSource;
+import net.sf.jasperreports.engine.export.ooxml.JRXlsxExporter;
+import net.sf.jasperreports.export.SimpleExporterInput;
+import net.sf.jasperreports.export.SimpleOutputStreamExporterOutput;
+import net.sf.jasperreports.export.SimpleXlsxReportConfiguration;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 
@@ -50,13 +58,20 @@ public class JasperReportsDocumentRenderer implements DocumentRenderer {
         InputStream jasperTemplateInputStream = new ByteArrayInputStream(jasperTemplateBytes);
 
         Map<String, Object> parameters = new HashMap<>();
-        if(subDocuments != null){
+        if (subDocuments != null) {
             fillSubDocumentParams(parameters, subDocuments);
         }
 
-        JsonDataSource dataSource = asJsonDataSource(data);
         try {
-            JasperPrint jasperPrint = JasperFillManager.fillReport(jasperTemplateInputStream, parameters, dataSource);
+            JasperPrint jasperPrint = new JasperPrint();
+            if (APPLICATION_PDF.equals(mediaType)) {
+                JsonDataSource jsonDataSource = asJsonDataSource(data);
+                jasperPrint = JasperFillManager.fillReport(jasperTemplateInputStream, parameters, jsonDataSource);
+            }
+            if (APPLICATION_OCTET_STREAM.equals(mediaType)) {
+                JRXlsxDataSource xlsxDataSource = asXlsxDataSource(data);
+                jasperPrint = JasperFillManager.fillReport(jasperTemplateInputStream, parameters, xlsxDataSource);
+            }
             return exportDocumentTo(jasperPrint, mediaType);
         } catch (JRException e) {
             String msg = String.format("Failed to render document with key '%s'", key);
@@ -82,7 +97,18 @@ public class JasperReportsDocumentRenderer implements DocumentRenderer {
         }
     }
 
+    private JRXlsxDataSource asXlsxDataSource(Object data) {
+        try {
+            return new JRXlsxDataSource(new ByteArrayInputStream(new ObjectMapper().writeValueAsBytes(data)));
+        } catch (JRException | IOException e) {
+            throw new DocumentRenderingException("Failed to parse document data as Excel. " + e.getMessage(), e);
+        }
+    }
+
     private byte[] exportDocumentTo(JasperPrint jasperPrint, MediaType mediaType) throws JRException {
+        if (APPLICATION_OCTET_STREAM.equals(mediaType)) {
+            return configureJRXlsxExporter(jasperPrint).toByteArray();
+        }
         if (APPLICATION_PDF.equals(mediaType)) {
             return JasperExportManager.exportReportToPdf(jasperPrint);
         }
@@ -90,6 +116,19 @@ public class JasperReportsDocumentRenderer implements DocumentRenderer {
             return JasperExportManager.exportReportToXml(jasperPrint).getBytes(UTF_8);
         }
         throw new IllegalArgumentException(mediaType.toString() + " not supported by JasperReports");
+    }
+
+    private ByteArrayOutputStream configureJRXlsxExporter(JasperPrint jasperPrint) {
+        SimpleXlsxReportConfiguration configuration = new SimpleXlsxReportConfiguration();
+        configuration.setOnePagePerSheet(true);
+        configuration.setDetectCellType(true);
+        JRXlsxExporter exporter = new JRXlsxExporter();
+        exporter.setConfiguration(configuration);
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        exporter.setExporterInput(new SimpleExporterInput(jasperPrint));
+        exporter.setExporterOutput(new SimpleOutputStreamExporterOutput(baos));
+        exporter.getExporterOutput();
+        return baos;
     }
 
     @Override
